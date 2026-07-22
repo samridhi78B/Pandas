3a5ef4553e6686a22eb4a16d2a46d8199ce9b511
Samridhi Gumber <samridhigumber05@gmail.com>
2026-07-22 14:33:17 +0530

    Create right.py

---
+++ Merging_&_Joining/right.py
@@ -0,0 +1,19 @@
+import pandas as pd
+employees = pd.DataFrame({
+    "Emp_ID": [101, 102, 103, 104],
+    "Name": ["Aman", "Riya", "Karan", "Neha"],
+    "Department_ID": [1, 2, 3, 2]
+})
+
+departments = pd.DataFrame({
+    "Department_ID": [1, 2, 4],
+    "Department_Name": ["HR", "IT", "Finance"]
+})
+
+res=pd.merge(employees, departments, on="Department_ID", how="right")
+print(res)
+#    Emp_ID  Name  Department_ID Department_Name
+# 0   101.0  Aman              1              HR
+# 1   102.0  Riya              2              IT
+# 2   104.0  Neha              2              IT
+# 3     NaN   NaN              4         Finance
